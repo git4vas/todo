5abe3585817c2a45038eb0f69721b1099f70740f
Vasily Neganov <git4vas@example.com>
2023-01-14 15:34:06 +0000

    show test data

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const { text } = require('express');
-var express = require('express');
-var app = express()
+const express = require('express');
+const app = express()
+
+const entries = {
+    pk1: {text: 'hello', flag: false, parent: null},
+    pk2: {text: 'hello2', flag: true, parent: null}
+}
+
+
 
 app.get('/', function (req, res) {
   res.setHeader("Content-Type" , "text/plain")
   res.send('Hello World!' + new Date());
 });
+
+app.get('/entry', function (req, res) {
+  res.setHeader("Content-Type" , "application/json")
+  res.send(JSON.stringify(entries));
+});
+
 app.listen(8080, function () {
   console.log('Running on port 8080!')
 });
