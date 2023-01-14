94487d073608c3ed93e43c2d4caed999b346d37d
Vasily Neganov <git4vas@example.com>
2023-01-14 15:17:20 +0000

    install express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const { text } = require('express');
+var express = require('express');
+var app = express()
+
+app.get('/', function (req, res) {
+  res.setHeader("Content-Type" , "text/plain")
+  res.send('Hello World!' + new Date());
+});
+app.listen(8080, function () {
+  console.log('Running on port 8080!')
+});
