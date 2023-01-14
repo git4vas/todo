44305b5e0d712f350775e1dc6aaf0a9ca88fdf1d
Vasily Neganov <git4vas@example.com>
2023-01-14 17:41:58 +0000

    setup linter

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
-const app = express()
+const app = express();
 
 const entries = {
     pk1: { text: 'hello', flag: false, parent: null },
     pk2: { text: 'hello2', flag: true, parent: null },
     pk3: { text: 'hello2', flag: true, parent: 'pk1' }
-}
+};
 
 
 
 app.get('/', function (req, res) {
-  res.setHeader("Content-Type" , "text/plain")
-  res.send('Hello World!' + new Date());
+    res.setHeader('Content-Type' , 'text/plain');
+    res.send('Hello World!' + new Date());
 });
 
 app.get('/entry', function (req, res) {
-  res.setHeader("Content-Type" , "application/json")
-  res.send(JSON.stringify(entries));
+    res.setHeader('Content-Type' , 'application/json');
+    res.send(JSON.stringify(entries));
 });
 
 app.get('/entry/:key', function (req, res) {
-  res.setHeader("Content-Type" , "application/json")
-  res.send(JSON.stringify(entries[req.params.key]));
+    res.setHeader('Content-Type' , 'application/json');
+    res.send(JSON.stringify(entries[req.params.key]));
 });
 
 
 
 app.listen(8080, function () {
-  console.log('Running on port 8080!')
+    console.log('Running on port 8080!');
 });
