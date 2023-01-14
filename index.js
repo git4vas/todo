6412874c104b2d24ce2a1c2934b6df147ac84a2e
Vasily Neganov <git4vas@example.com>
2023-01-14 16:45:56 +0000

    get single entries

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express()
 
 const entries = {
-    pk1: {text: 'hello', flag: false, parent: null},
-    pk2: {text: 'hello2', flag: true, parent: null}
+    pk1: { text: 'hello', flag: false, parent: null },
+    pk2: { text: 'hello2', flag: true, parent: null },
+    pk3: { text: 'hello2', flag: true, parent: 'pk1' }
 }
 
 
@@ -17,6 +18,13 @@ app.get('/entry', function (req, res) {
   res.setHeader("Content-Type" , "application/json")
   res.send(JSON.stringify(entries));
 });
+
+app.get('/entry/:key', function (req, res) {
+  res.setHeader("Content-Type" , "application/json")
+  res.send(JSON.stringify(entries[req.params.key]));
+});
+
+
 
 app.listen(8080, function () {
   console.log('Running on port 8080!')
