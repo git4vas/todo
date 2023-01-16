116cafab985ecf18f0fac7d3fa1a8f9a7faa269c
Vasily Neganov <git4vas@example.com>
2023-01-16 17:05:03 +0000

    read data from file, install airbnb lint

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 
-const entries = {
-    pk1: { text: 'hello', flag: false, parent: null },
-    pk2: { text: 'hello2', flag: true, parent: null },
-    pk3: { text: 'hello2', flag: true, parent: 'pk1' }
+
+let entries = {};
+
+const readData = (err, data) => {
+    if(err){
+        console.error(err);
+    }
+    else {
+        console.log(data);
+        entries = JSON.parse(data);        
+    }
 };
 
 
 
+
+fs.readFile('./data/list.json', 'utf-8', readData);
+
+//const entries = {
+//    pk1: { text: 'hello', flag: false, parent: null },
+//    pk2: { text: 'hello2', flag: true, parent: null },
+//    pk3: { text: 'hello2', flag: true, parent: 'pk1' }
+//};
+
+
+
 app.get('/', function (req, res) {
-    res.setHeader('Content-Type' , 'text/plain');
-    res.send('Hello World!' + new Date());
+    res.setHeader('Content-Type', 'text/plain')
+        .status(200)
+        .send('Hello World! now it\'s ' + new Date());
+    //res.status(200).json('something else');
 });
 
 app.get('/entry', function (req, res) {
-    res.setHeader('Content-Type' , 'application/json');
-    res.send(JSON.stringify(entries));
+    res.setHeader('Content-Type', 'application/json')
+        .send(JSON.stringify(entries));
+    console.log(entries);
+    console.log(req.query);    
 });
 
 app.get('/entry/:key', function (req, res) {
-    res.setHeader('Content-Type' , 'application/json');
+    res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(entries[req.params.key]));
 });
 
--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
 # todo
+
+```sh
+npm init
+npm install express
+```
+
+
+
+### linter
+
+[npm](https://www.npmjs.com/package/eslint#installation-and-usage)
+[airbnb](https://www.npmjs.com/package/eslint-config-airbnb-base)
+
+```sh
+npm init @eslint/config
+./node_modules/.bin/eslint yourfile.js
+```
+
+### postman
+
+[postman](https://learning.postman.com/docs/getting-started/installation-and-updates/#installing-postman-on-linux)
