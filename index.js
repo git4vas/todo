73a1135f8d1d460eb8274a77ec6e3242a511d34e
vsn9 <96695116+git4vas@users.noreply.github.com>
2023-02-11 16:23:50 +0000

    fix lint

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const service = require('./src/service');
 
 // TODO change to promise in model
 const readData = (err, data) => {
-  if (err) {
-    console.error(err);
-  }
-  else {
-    console.log('read from file:\n', data);
-    try {
-      model.setEntries(JSON.parse(data));
-      console.log('data parsed');
-      service.setModel(model);
-      service.startServer(80);
-      // TODO process.env.PORT || 8080
+    if (err) {
+        console.error(err);
     }
-    catch (errJSON) {
-      console.error(errJSON);
+    else {
+        console.log('read from file:\n', data);
+        try {
+            model.setEntries(JSON.parse(data));
+            console.log('data parsed');
+            service.setModel(model);
+            service.startServer(80);
+            // TODO process.env.PORT || 8080
+        }
+        catch (errJSON) {
+            console.error(errJSON);
+        }
     }
-  }
 };
 
 
